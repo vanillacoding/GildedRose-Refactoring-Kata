cf4493adadff2bdbc7ef4864f29e9cd7accbb223
Eun0713 <daeunlee0713@naver.com>
2025-04-28 11:34:36 +0900

    Style: 코드 스타일 수정

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,7 +7,7 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
