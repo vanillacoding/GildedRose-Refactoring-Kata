65cfa9145378802094ce9613d0964d01b04498b5
ben0429 <hadeskim0429@gmail.com>
2025-04-28 11:11:17 +0900

    Refactor: 함수명을 명확하게 수정

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -10,14 +10,14 @@ class Shop {
     this.items = items;
   }
 
-  isItemSulfras(itemName) {
+  itemNotBelongToSulfuras(itemName) {
     if (itemName != 'Sulfuras, Hand of Ragnaros') {
       return true;
     } else {
       return false;
     }
   }
-  isItemAgedBrie(itemName) {
+  itemNotBelongToAgedBrie(itemName) {
     if (itemName != 'Aged Brie') {
       return true;
     } else {
@@ -34,8 +34,8 @@ class Shop {
 
   itemQualityAvailable(item) {
     if (
-      itemQuality > 0
-      && this.isItemSulfras(item.name)
+      item.quality > 0
+      && this.itemNotBelongToSulfuras(item.name)
     ) {
       this.decreaseItemQuality(item);
     }
@@ -45,38 +45,38 @@ class Shop {
     for (var i = 0; i < this.items.length; i++) {
       const itemName = this.items[i].name;
 
-      if (isItemAgedBrie(itemName) && itemName != 'Backstage passes to a TAFKAL80ETC concert') {
-        this.itemQualityAvailable(items, i);
+      if (itemNotBelongToAgedBrie(itemName) && itemName != 'Backstage passes to a TAFKAL80ETC concert') {
+        this.itemQualityAvailable(items[i]);
       } else {
         if (this.items[i].quality < 50) {
-          this.increaseItemQuality(this.items[i].quality);
+          this.increaseItemQuality(this.items[i]);
           if (itemName == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
-                this.increaseItemQuality(this.items[i].quality);
+                this.increaseItemQuality(this.items[i]);
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
-                this.increaseItemQuality(this.items[i].quality);
+                this.increaseItemQuality(this.items[i]);
               }
             }
           }
         }
       }
-      if (this.isItemSulfras(itemName)) {
+      if (this.itemNotBelongToSulfuras(itemName)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.isItemAgedBrie(itemName)) {
+        if (this.itemNotBelongToAgedBrie(itemName)) {
           if (itemName != 'Backstage passes to a TAFKAL80ETC concert') {
-            this.itemQualityAvailable(items, i);
+            this.itemQualityAvailable(items[i]);
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
           if (this.items[i].quality < 50) {
-            this.increaseItemQuality(this.items[i].quality);
+            this.increaseItemQuality(this.items[i]);
           }
         }
       }
