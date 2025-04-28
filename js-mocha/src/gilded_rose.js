3da14098463e91dd97987bd4e23ce384bd6e21b5
leesm0218 <leesm0218@naver.com>
2025-04-28 09:49:13 +0900

    if (true) 제거

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -14,25 +14,17 @@ class Shop {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          if (true) {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+          this.items[i].quality = this.items[i].quality - 1;
         }
       } 
       if(this.items[i].name == 'Aged Brie' || this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (true) {
-            if (this.items[i].sellIn < 11) {
-              if (true) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (true) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
+          if (this.items[i].sellIn < 11) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+          if (this.items[i].sellIn < 6) {
+            this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
