5b0aa9be6b2c38373315a6ecd724abaefa114d45
leesm0218 <leesm0218@naver.com>
2025-04-28 11:03:56 +0900

    완전히 같은, 두 독립된 if 문의 내용 통합

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -16,14 +16,10 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].name != 'Aged Brie') {
-        if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert'
-          && this.items[i].quality > 0
-          && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          this.items[i].quality = this.items[i].quality - 1;
-        }
         if (this.items[i].quality > 0
           && this.items[i].name != 'Sulfuras, Hand of Ragnaros'
           && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+          this.items[i].quality = this.items[i].quality - 1;
           this.items[i].quality = this.items[i].quality - 1;
         }
         if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
