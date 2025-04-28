0cfd7a00a8e1a1dd9e4bcceeed0cbcb70240b948
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-04-28 11:27:21 +0900

    chore: 주석 제거

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -20,40 +20,28 @@ class Shop {
       const backstagePass = "Backstage passes to a TAFKAL80ETC concert";
 
       if (itemName != sulfuras) {
-        // 전설 아이템 제외 전부 남은 기간 -1일
         timeLeft -= 1;
 
-        if (
-          itemName != agedBrie &&
-          itemName != backstagePass // 브리치즈와 패스 제외 아이템
-        ) {
+        if (itemName != agedBrie && itemName != backstagePass) {
           if (itemQuality > 0) {
-            // 0원 되기 전까지 1원 내리기
             itemQuality -= 1;
             if (timeLeft < 0) {
-              // 남은 기간 음수일 시
               if (itemQuality > 0) {
-                // 추가로 1원 내리기, 총 2원 내리기
                 itemQuality -= 1;
               } else {
-                itemQuality == 0; // 0원일 시 그대로 값은 0원
+                itemQuality == 0;
               }
             }
           }
         } else {
           if (itemQuality < 50) {
-            // 1. 브리치즈의 경우
-            itemQuality += 1; // 1원만 올리기
+            itemQuality += 1;
             if (itemName == backstagePass) {
-              // 2. 패스인 경우
               if (timeLeft < 11) {
-                // 남은 기간 11일 미만일 시, 추가 1원 올리기, 총 2원 올리기
                 itemQuality += 1;
               } else if (timeLeft < 6) {
-                // 남은 기간 6일 미만일 시, 추가 1원 올리기, 총 3원 올리기
                 itemQuality += 1;
               } else if (timeLeft == 0) {
-                // 남은 기간 0일일 시, 값은 0원
                 itemQuality == 0;
               }
             }
