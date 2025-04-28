0bcbb357e51f28646a22fe163646a41833c24c5a
ben0429 <hadeskim0429@gmail.com>
2025-04-28 10:58:57 +0900

    Refactor: 아이템 구분 함수로 분리 및 Item Quality가 존재시 로직을 함수로 분리

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -10,32 +10,47 @@ class Shop {
     this.items = items;
   }
 
-  sulfurasItemCheck(itemName) {
+  isItemSulfras(itemName) {
     if (itemName != 'Sulfuras, Hand of Ragnaros') {
       return true;
     } else {
       return false;
     }
   }
-  increaseItemQuality(itemQuality) {
-    itemQuality += 1;
+  isItemAgedBrie(itemName) {
+    if (itemName != 'Aged Brie') {
+      return true;
+    } else {
+      return false;
+    }
   }
-  decreaseItemQuality(itemQuality) {
-    itemQuality -= 1;
+
+  increaseItemQuality(item) {
+    item.quality += 1;
+  }
+  decreaseItemQuality(item) {
+    item.quality -= 1;
+  }
+
+  itemQualityAvailable(item) {
+    if (
+      itemQuality > 0
+      && this.isItemSulfras(item.name)
+    ) {
+      this.decreaseItemQuality(item);
+    }
   }
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.sulfurasItemCheck(this.items[i].name)) {
-            this.decreaseItemQuality(this.items[i].quality);
-          }
-        }
+      const itemName = this.items[i].name;
+
+      if (isItemAgedBrie(itemName) && itemName != 'Backstage passes to a TAFKAL80ETC concert') {
+        this.itemQualityAvailable(items, i);
       } else {
         if (this.items[i].quality < 50) {
           this.increaseItemQuality(this.items[i].quality);
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (itemName == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.increaseItemQuality(this.items[i].quality);
@@ -49,17 +64,13 @@ class Shop {
           }
         }
       }
-      if (this.sulfurasItemCheck(this.items[i].name)) {
+      if (this.isItemSulfras(itemName)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.sulfurasItemCheck(this.items[i].name)) {
-                this.decreaseItemQuality(this.items[i].quality);
-              }
-            }
+        if (this.isItemAgedBrie(itemName)) {
+          if (itemName != 'Backstage passes to a TAFKAL80ETC concert') {
+            this.itemQualityAvailable(items, i);
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
