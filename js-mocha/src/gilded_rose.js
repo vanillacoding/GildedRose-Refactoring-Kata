7949f22cb87fe500822706a475e572887bbeda6c
leesm0218 <leesm0218@naver.com>
2025-04-28 09:37:08 +0900

    조건문 단순화를 통해 true 만 남는 조건문들을 식별

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -13,8 +13,8 @@ class Shop {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (true) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
@@ -23,12 +23,12 @@ class Shop {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
+              if (true) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
+              if (true) {
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
@@ -41,8 +41,8 @@ class Shop {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (true) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
