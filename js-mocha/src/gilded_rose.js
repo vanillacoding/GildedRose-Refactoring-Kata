6fe4be34397eb97dfd9040f5e1dea7ebcf7a2a6c
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-04-28 11:23:57 +0900

    refactor

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,51 +7,56 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+    for (let i = 0; i < this.items.length; i++) {
+      let timeLeft = this.items[i].sellIn;
+      let itemQuality = this.items[i].quality;
+      let itemName = this.items[i].name;
+      const sulfuras = "Sulfuras, Hand of Ragnaros";
+      const agedBrie = "Aged Brie";
+      const backstagePass = "Backstage passes to a TAFKAL80ETC concert";
+
+      if (itemName != sulfuras) {
+        // 전설 아이템 제외 전부 남은 기간 -1일
+        timeLeft -= 1;
+
+        if (
+          itemName != agedBrie &&
+          itemName != backstagePass // 브리치즈와 패스 제외 아이템
+        ) {
+          if (itemQuality > 0) {
+            // 0원 되기 전까지 1원 내리기
+            itemQuality -= 1;
+            if (timeLeft < 0) {
+              // 남은 기간 음수일 시
+              if (itemQuality > 0) {
+                // 추가로 1원 내리기, 총 2원 내리기
+                itemQuality -= 1;
+              } else {
+                itemQuality == 0; // 0원일 시 그대로 값은 0원
               }
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (itemQuality < 50) {
+            // 1. 브리치즈의 경우
+            itemQuality += 1; // 1원만 올리기
+            if (itemName == backstagePass) {
+              // 2. 패스인 경우
+              if (timeLeft < 11) {
+                // 남은 기간 11일 미만일 시, 추가 1원 올리기, 총 2원 올리기
+                itemQuality += 1;
+              } else if (timeLeft < 6) {
+                // 남은 기간 6일 미만일 시, 추가 1원 올리기, 총 3원 올리기
+                itemQuality += 1;
+              } else if (timeLeft == 0) {
+                // 남은 기간 0일일 시, 값은 0원
+                itemQuality == 0;
+              }
+            }
           }
         }
       }
@@ -62,5 +67,5 @@ class Shop {
 }
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
