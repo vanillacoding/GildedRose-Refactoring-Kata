9d81da8f02d53491660fc2ef2015dd308f0e9782
ChaJunWoo18 <wnsdnxla@naver.com>
2025-04-28 10:36:10 +0900

    반복문 - 파이프라인으로 바꾸기

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -11,49 +11,47 @@ class Shop {
     this.items = items;
   }
 
-  updateAgedBrie(items) {
-    if (items.sellIn < 0) {
-      items.quality = items.quality - 1;
-    } else if (items.sellIn > 0 && items.quality < 50) {
-      items.quality = items.quality + 1;
+  updateAgedBrie(item) {
+    if (item.sellIn < 0) {
+      item.quality = item.quality - 1;
+    } else if (item.sellIn > 0 && item.quality < 50) {
+      item.quality = item.quality + 1;
     }
   }
 
-  updateBackStagePasses(items) {
-    if (items.sellIn < 0) {
-      items.quality = items.quality - items.quality;
-    } else if (items.quality > 0 && items.quality < 50) {
-      items.quality = items.quality + 1;
+  updateBackStagePasses(item) {
+    if (item.sellIn < 0) {
+      item.quality = item.quality - item.quality;
+    } else if (item.quality > 0 && item.quality < 50) {
+      item.quality = item.quality + 1;
 
-      if (items.sellIn < 11) {
-        items.quality = items.quality + 1;
+      if (item.sellIn < 11) {
+        item.quality = item.quality + 1;
       }
 
-      if (items.sellIn < 6) {
-        items.quality = items.quality + 1;
+      if (item.sellIn < 6) {
+        item.quality = item.quality + 1;
       }
     }
   }
 
-  updateNomalItem(items) {
-    items.quality = items.quality - 1;
-    if (items.sellIn < 0) {
-      items.quality = items.quality - 1;
+  updateNormalItem(item) {
+    item.quality = item.quality - 1;
+    if (item.sellIn < 0) {
+      item.quality = item.quality - 1;
     }
   }
 
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name === 'Aged Brie') {
-        this.updateAgedBrie(this.items[i]);
-      } else if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
-        this.updateBackStagePasses(this.items[i]);
-      } else if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-        this.updateNomalItem(this.items[i]);
+    return this.items.map((item) => {
+      if (item.name === 'Aged Brie') {
+        this.updateAgedBrie(item);
+      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        this.updateBackStagePasses(item);
+      } else if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        this.updateNomalItem(item);
       }
-    }
-
-    return this.items;
+    });
   }
 
 }
