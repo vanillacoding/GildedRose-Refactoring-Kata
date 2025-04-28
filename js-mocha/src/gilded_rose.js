f9780cde8750505fe80dc9c3b93ff70ce54adfdc
leesm0218 <leesm0218@naver.com>
2025-04-28 11:02:49 +0900

    this.items[i].quality < 50 조건문 통합

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -32,20 +32,16 @@ class Shop {
         }
       }
       if (this.items[i].name == 'Aged Brie'
-        || this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality < 50) {
+        || this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'
+        && this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
+        if (this.items[i].sellIn < 11) {
           this.items[i].quality = this.items[i].quality + 1;
         }
-        if (this.items[i].quality < 50
-          && this.items[i].sellIn < 11) {
-          this.items[i].quality = this.items[i].quality + 1;
-        }
-        if (this.items[i].quality < 50
-          && this.items[i].sellIn < 6) {
+        if (this.items[i].sellIn < 6) {
           this.items[i].quality = this.items[i].quality + 1;
         }
         if (this.items[i].name == 'Aged Brie'
-          && this.items[i].quality < 50
           && this.items[i].sellIn < 0) {
           this.items[i].quality = this.items[i].quality + 1;
         }
