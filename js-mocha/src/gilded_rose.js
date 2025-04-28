fe39fc5643e421c58d243fa56b3d2d15999bc61c
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-04-28 11:31:36 +0900

    refactor: js-mocha 리팩토링

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -6,52 +6,92 @@ class Item {
   }
 }
 
+// 아이템 유형 판별
+class ItemRule {
+  static isAgedBrie (item) {
+    return item.name === 'Aged Brie';
+  }
+
+  static isBackstagePass(item) {
+    return item.name === 'Backstage passes to a TAFKAL80ETC concert';
+  }
+
+  static isSulfuras(item) {
+    return item.name === 'Sulfuras, Hand of Ragnaros';
+  }
+
+  static minusSellin(item) {
+    return item.sellIn -= 1;
+  }
+}
+
+// Quility 계산
+class QuilityRule {
+  static plusQuality(item) {
+    item.quality += 1;
+  }
+
+  static minusQuality(item) {
+    item.quality -= 1;
+  }
+
+  static IsOverZero(item) {
+    return item.quality > 0
+  }
+
+  static IsUnderfifty(item) {
+    return item.quality < 50
+  }
+}
+
+// Shop
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+      if (ItemRule.isAgedBrie(this.items[i]) && ItemRule.isBackstagePass(this.items[i])) {
+        if (QuilityRule.IsOverZero(this.items[i]) && ItemRule.isSulfuras(this.items[i])) {
+          QuilityRule.plusQuality(this.items[i]);
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (QuilityRule.IsUnderfifty(this.items[i])) {
+          QuilityRule.plusQuality(this.items[i]);
+          if (ItemRule.isBackstagePass(this.items[i])) {
             if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+              if (QuilityRule.IsUnderfifty(this.items[i])) {
+                QuilityRule.plusQuality(this.items[i])
               }
             }
             if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+              if (QuilityRule.IsUnderfifty(this.items[i])) {
+                QuilityRule.plusQuality(this.items[i]);
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+      if (ItemRule.isSulfuras(this.items[i])) {
+        ItemRule.minusSellin(this.items[i]);
       }
+
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+        if (ItemRule.isAgedBrie(this.items[i])) {
+          if (ItemRule.isBackstagePass(this.items[i])) {
+            if (QuilityRule.IsOverZero(this.items[i])) {
+              if (ItemRule.isSulfuras(this.items[i])) {
+                QuilityRule.minusQuality(this.items[i]);
               }
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
+          if (QuilityRule.IsUnderfifty(this.items[i])) {
+            QuilityRule.plusQuality(this.items[i]);
           }
         }
       }
@@ -60,6 +100,10 @@ class Shop {
     return this.items;
   }
 }
+
+const items = [new Item("", 123, 10), new Item("", 789, 11)];
+const shops = new Shop(items);
+
 module.exports = {
   Item,
   Shop
