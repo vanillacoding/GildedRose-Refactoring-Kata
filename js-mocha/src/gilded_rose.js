e5cf48435c198d47be2ba5da69f797c5357e9c21
ChaJunWoo18 <wnsdnxla@naver.com>
2025-04-28 11:28:05 +0900

    중복 코드 제거하기(함수추출)

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -11,45 +11,50 @@ class Shop {
     this.items = items;
   }
 
+  increaseQuality(item) {
+    item.quality += 1;
+  }
+
+  decreaseQulity(item, value) {
+    item.quality -= value;
+  }
+
   updateAgedBrie(item) {
-    if (item.sellIn < 0) {
-      item.quality = item.quality - 1;
-    } else if (item.sellIn > 0 && item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
+    this.increaseQuality(item);
   }
 
   updateBackStagePasses(item) {
     if (item.sellIn < 0) {
       item.quality = item.quality - item.quality;
-    } else if (item.quality > 0 && item.quality < 50) {
-      item.quality = item.quality + 1;
+    } else {
+      this.increaseQuality(item);
 
       if (item.sellIn < 11) {
-        item.quality = item.quality + 1;
+        this.increaseQuality(item);
       }
 
       if (item.sellIn < 6) {
-        item.quality = item.quality + 1;
+        this.increaseQuality(item);
       }
     }
   }
 
   updateNormalItem(item) {
-    item.quality = item.quality - 1;
-    if (item.sellIn < 0) {
-      item.quality = item.quality - 1;
-    }
+    item.quality = item.sellIn < 0
+      ? this.decreaseQulity(item, 2)
+      : this.decreaseQulity(item, 1);
   }
 
   updateQuality() {
     return this.items.map((item) => {
-      if (item.name === 'Aged Brie') {
-        this.updateAgedBrie(item);
-      } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-        this.updateBackStagePasses(item);
-      } else if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-        this.updateNomalItem(item);
+      if (item.quality < 50) {
+        if (item.name === 'Aged Brie') {
+          this.updateAgedBrie(item);
+        } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+          this.updateBackStagePasses(item);
+        } else if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+          this.updateNormalItem(item);
+        }
       }
     });
   }
