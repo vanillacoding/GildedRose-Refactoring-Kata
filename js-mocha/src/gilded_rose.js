c3a94d1582457e6c868f47751201a5a1280fb2e7
leesm0218 <leesm0218@naver.com>
2025-04-28 10:01:44 +0900

    if (true) 제거

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -36,9 +36,9 @@ class Shop {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              if (true) {
+              
                 this.items[i].quality = this.items[i].quality - 1;
-              }
+              
             }
           }
         }
@@ -48,9 +48,9 @@ class Shop {
           }
         }
         if (this.items[i].name == 'Aged Brie' && this.items[i].quality < 50) {
-          if (true) {
+          
             this.items[i].quality = this.items[i].quality + 1;
-          }
+          
         }
       }
     }
